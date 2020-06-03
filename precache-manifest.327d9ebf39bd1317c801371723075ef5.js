b8f2cfdb5d459f872f9c663c0f91b2459c84fdf7
dev.rayavaram <dev.rayavaram@gmail.com>
2020-06-03 13:56:11 -0500

    Updates

---
+++ precache-manifest.327d9ebf39bd1317c801371723075ef5.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100456a5456441d27858aca566810263",
+    "url": "/artStoreDev/index.html"
+  },
+  {
+    "revision": "82d73903ef8f5999bfca",
+    "url": "/artStoreDev/static/css/main.15ac9e77.chunk.css"
+  },
+  {
+    "revision": "28d508cec43778754c58",
+    "url": "/artStoreDev/static/js/2.8d5b8c5f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/artStoreDev/static/js/2.8d5b8c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d73903ef8f5999bfca",
+    "url": "/artStoreDev/static/js/main.5e5ec2f1.chunk.js"
+  },
+  {
+    "revision": "eee5b3d385b123f4b4d7",
+    "url": "/artStoreDev/static/js/runtime-main.51acaa0b.js"
+  },
+  {
+    "revision": "0b5f81320687eb47ecffbf4b0375d67f",
+    "url": "/artStoreDev/static/media/bannerImage.0b5f8132.jpg"
+  },
+  {
+    "revision": "424b99278dcb95688d9382d78e1d4b5c",
+    "url": "/artStoreDev/static/media/cart.424b9927.jpeg"
+  },
+  {
+    "revision": "31f51b53ddc26f15b80d43347276149f",
+    "url": "/artStoreDev/static/media/imag.31f51b53.jpg"
+  },
+  {
+    "revision": "99e48335b3e55544417beebcdf2a2ce2",
+    "url": "/artStoreDev/static/media/image-lg-1.99e48335.jpg"
+  },
+  {
+    "revision": "0cc313b7f16e02ac8831ab733ebc476d",
+    "url": "/artStoreDev/static/media/image-lg-2.0cc313b7.jpg"
+  },
+  {
+    "revision": "1817e768c160de19ad4f8cbdf8317023",
+    "url": "/artStoreDev/static/media/image-lg-3.1817e768.jpg"
+  },
+  {
+    "revision": "df8df01ac45d96407de1cad6a7cd8501",
+    "url": "/artStoreDev/static/media/image-lg-4.df8df01a.jpg"
+  },
+  {
+    "revision": "c1a512d5ff0f3873543cdf289f0daa06",
+    "url": "/artStoreDev/static/media/image-lg-5.c1a512d5.jpg"
+  },
+  {
+    "revision": "554e46940a4a1d284feedc47dca45ae0",
+    "url": "/artStoreDev/static/media/image-lg-7.554e4694.jpg"
+  },
+  {
+    "revision": "4ddf3a13cf511d7751bf3cff3f343234",
+    "url": "/artStoreDev/static/media/image-lg-9.4ddf3a13.jpg"
+  },
+  {
+    "revision": "b44b9cc553a5de19ea63243f5739b935",
+    "url": "/artStoreDev/static/media/image-sm-1.b44b9cc5.jpg"
+  },
+  {
+    "revision": "0cc313b7f16e02ac8831ab733ebc476d",
+    "url": "/artStoreDev/static/media/image-sm-2.0cc313b7.jpg"
+  },
+  {
+    "revision": "eb5498eef3996c0d3ab468a38bdb1e83",
+    "url": "/artStoreDev/static/media/image-sm-3.eb5498ee.jpg"
+  },
+  {
+    "revision": "df8df01ac45d96407de1cad6a7cd8501",
+    "url": "/artStoreDev/static/media/image-sm-4.df8df01a.jpg"
+  },
+  {
+    "revision": "c1a512d5ff0f3873543cdf289f0daa06",
+    "url": "/artStoreDev/static/media/image-sm-5.c1a512d5.jpg"
+  },
+  {
+    "revision": "614c9e475264b61b8f4d19a07dac1dd3",
+    "url": "/artStoreDev/static/media/image-sm-7.614c9e47.jpg"
+  },
+  {
+    "revision": "4ddf3a13cf511d7751bf3cff3f343234",
+    "url": "/artStoreDev/static/media/image-sm-9.4ddf3a13.jpg"
+  },
+  {
+    "revision": "16956f9255b9e7ba4fdc9764421b068e",
+    "url": "/artStoreDev/static/media/page.16956f92.jpeg"
+  },
+  {
+    "revision": "36bd2938de78af8cc36bc1aea3187997",
+    "url": "/artStoreDev/static/media/pexels-photo-1037993.36bd2938.jpeg"
+  },
+  {
+    "revision": "a7f36ff0f230df2e7223ee70f619536d",
+    "url": "/artStoreDev/static/media/pexels-photo-1646953.a7f36ff0.jpeg"
+  },
+  {
+    "revision": "007f19844ce0572cd45446a49ab8747b",
+    "url": "/artStoreDev/static/media/pexels-photo-220320.007f1984.jpeg"
+  },
+  {
+    "revision": "d77c88d4a2140b7ac1779cdb96730f80",
+    "url": "/artStoreDev/static/media/pexels-photo-743986.d77c88d4.jpeg"
+  },
+  {
+    "revision": "30c1228bcd107c7d5d772ad593511685",
+    "url": "/artStoreDev/static/media/tajmahal-canvas-art.30c1228b.jpg"
+  }
+]);
